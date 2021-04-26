1b17a6491d3f51ee99df6d5ebc93836277d8ba79
James Burton <jamesejburton@gmail.com>
2021-04-26 05:08:24 +0100

    Added more sounds

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
-LSM:Register("sound", "|cff7f58dbBeware of Yeti|r", [[Interface\Addons\ShardsSoundPack\sound\Beware of Yeti.ogg]])
-LSM:Register("sound", "|cff7f58dbCome Shop Browse|r", [[Interface\Addons\ShardsSoundPack\sound\Come Shop Browse.ogg]])
 LSM:Register("sound", "|cff7f58dbSkovald|r", [[Interface\Addons\ShardsSoundPack\sound\Skovald.ogg]])
+LSM:Register("sound", "|cff7f58dbBeware of Yeti|r", [[Interface\Addons\ShardsSoundPack\sound\Beware of Yeti.ogg]])
+LSM:Register("sound", "|cff7f58dbYour Day|r", [[Interface\Addons\ShardsSoundPack\sound\Your Day.ogg]])
+LSM:Register("sound", "|cff7f58dbCome Shop Browse|r", [[Interface\Addons\ShardsSoundPack\sound\Come Shop Browse.ogg]])
+LSM:Register("sound", "|cff7f58dbStupid Fart|r", [[Interface\Addons\ShardsSoundPack\sound\Stupid Fart.ogg]])
+LSM:Register("sound", "|cff7f58dbChicken Brain|r", [[Interface\Addons\ShardsSoundPack\sound\Chicken Brain.ogg]])
+LSM:Register("sound", "|cff7f58dbFFS Mark|r", [[Interface\Addons\ShardsSoundPack\sound\FFS Mark.ogg]])
