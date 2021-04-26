61379ad2b145bed764e5e604404fca02fd414351
James Burton <jamesejburton@gmail.com>
2021-04-26 04:57:51 +0100

    Added some sounds

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
 LSM:Register("sound", "|cff7f58dbBeware of Yeti|r", [[Interface\Addons\ShardsSoundPack\sound\Beware of Yeti.ogg]])
+LSM:Register("sound", "|cff7f58dbCome Shop Browse|r", [[Interface\Addons\ShardsSoundPack\sound\Come Shop Browse.ogg]])
+LSM:Register("sound", "|cff7f58dbSkovald|r", [[Interface\Addons\ShardsSoundPack\sound\Skovald.ogg]])
